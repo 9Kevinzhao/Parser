7715a49c7519b1ae6804ef119d916f1b039123f5
9Kevinzhao <9kzhao@stpatrick.org>
2018-01-16 14:45:57 -0600

    Add files via upload

---
+++ electionParser.js
@@ -0,0 +1,11 @@
+function electionParser(array){
+    this.state=array[0];
+    this.county=array[1];
+    this.fips=array[3];
+    this.obama=array[4];
+    this.pobama=array[5];
+    this.clinton=array[6];
+    this.pclinton=array[7];
+
+    
+}
