193c2b01f3b21ff9f168dcdae90ebc0cccf66f7a
9Kevinzhao <9kzhao@stpatrick.org>
2018-01-22 14:46:28 -0600

    Add files via upload

--- votingClient.js
+++ votingClient.js
@@ -38,15 +38,34 @@ function nameandpercent(array){
 function sortbypercentdifference(list){
 
     list.sort(function(a,b){return (a.pobama-a.promney)-(b.pobama-b.promney)})
+    return list[0];
 }
 
+function sortbyliteraldifference(list){
+    list.sort(function(a,b){return (a.obama-a.romney)-(b.obama-b.romney)})
+    return list[0]
+}
 
+function findStatesWithAtLeast(number){
+    let countycount = new Array(50);
+    let x=0
+    let index
+    countycount.fill(0);
+    for(i=0;i<list.length;i++){
+        if(!list[i].state=list[i-1].state){
+            x++;
+        }
+        countycount[x]++;
+    }
+    index=countycount.indexOf(countycount.find(number))
+
+}
 console.log(list[0]);
 console.log(list[500]);
 console.log(list[1000]);
 console.log(list[2000]);
 
-// find states with at least/at most/exactly blank counties
+// find states with at least/at most/exactly "number" counties
 // find all state totals aka fips ==0
 // find all states where percent difference was less than p%
 // sort counties within a given state from least to greatest total votes
